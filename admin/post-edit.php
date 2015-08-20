339a4955c50974948faf3d642308c90646fea939
pippintook <pippintook100@gmail.com>
2015-08-21 09:24:59 +1000

    Update post-edit.php

--- admin/post-edit.php
+++ admin/post-edit.php
@@ -1,3 +1,23 @@
 <html>
-<head>
-<title>
+  <head>
+    <title>Insert Post</title>
+  </head>
+<body>
+  <p>
+      <input type="text" name="post-title" />
+    </p>
+    <p>
+      <textarea name="post-content"></textarea>
+    </p>
+    <p>
+      <label for="post-category">Cat 1</label> 
+      <input type="checkbox" name="post-category" value="cat1" />
+      <br />
+      <label for="post-category">Cat 2</label> 
+      <input type="checkbox" name="post-category" value="cat2" />
+    </p>
+    <p>
+      <input type="submit" value="Submit" />
+    </p>
+</body>
+</html>
